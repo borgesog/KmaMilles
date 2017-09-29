49c254aa6ae674bba76a5f41cef8d16dda083648
Jordi Ogños <jordiognosaniceto@gmail.com>
2017-09-29 16:18:02 +0200

    Pràctica KmaMilles

---
+++ app/src/main/java/edu/upc/eseiaat/pma/kmamilles/MainActivity.java
@@ -0,0 +1,73 @@
+package edu.upc.eseiaat.pma.kmamilles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.view.menu.ActionMenuItemView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn_a_millas = (Button) findViewById(R.id.btn_a_millas);
+        Button btn_a_km = (Button) findViewById(R.id.btn_a_km);
+
+        final EditText edit_km = (EditText)  findViewById(R.id.edit_km);
+        final EditText edit_millas = (EditText) findViewById(R.id.edit_millas);
+
+
+       /* edit_millas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String skm = edit_km.getText().toString();
+                float km = Float.parseFloat(skm);
+                float millas = km/1.609f;
+                String smillas = String.format("%f",millas);
+                edit_km.setText(smillas);
+
+            }
+        });
+        edit_km.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String smillas = edit_millas.getText().toString();
+                float millas = Float.parseFloat(smillas);
+                float km = millas * 1.689f;
+
+                String skm = String.format("%f",km);
+                edit_millas.setText(skm);
+            }
+        });*/
+
+       btn_a_millas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String skm = edit_km.getText().toString();
+                float km = Float.parseFloat(skm);
+                float millas = km/1.609f;
+                String smillas = String.format("%f",millas);
+                edit_millas.setText(smillas);
+
+            }
+        });
+        btn_a_km.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String smillas = edit_millas.getText().toString();
+                float millas = Float.parseFloat(smillas);
+                float km = millas * 1.689f;
+
+                String skm = String.format("%f",km);
+                edit_km.setText(skm);
+            }
+        });
+
+
+    }
+}
